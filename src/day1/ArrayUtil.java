7b069caec542d3403818f6c72b340165d41dc9ed
RainbowSea <2969377330@qq.com>
2022-10-12 13:33:47 +0800

    测试版本回退

--- src/day1/ArrayUtil.java
+++ src/day1/ArrayUtil.java
@@ -121,6 +121,7 @@ class ArrayTest{
             int temp = arr[i];
             arr[i] = arr[arr.length-i-1];
             arr[arr.length-i-1] = temp;
+            System.out.println("");
         }
     }
 
